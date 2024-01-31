cdf925736572f8444019a1ae6b68e00fc6341f4c
Emily <emily.zy.lin@gmail.com>
2024-01-31 11:06:37 -0600

    added a button for the explore bar, added ExporeTopEvents to app.tsx

--- 03-frontend/react-event/src/App.tsx
+++ 03-frontend/react-event/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from './layouts/NavBarAndFooter/Navbar';
+import { ExploreTopEvents } from './layouts/HomePage/ExploreTopEvents';
 
 function App() {
   return (
-  <Navbar />
-    
+    <div>
+      <Navbar />
+      <ExploreTopEvents />    
+    </div>
+    //must have the div tag for App.tsx
   );
 }
 
