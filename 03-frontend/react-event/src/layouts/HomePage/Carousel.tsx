27e4f1906b426e1c2d0db59166235e31cc940cec
Emily <emily.zy.lin@gmail.com>
2024-01-31 17:44:26 -0600

    added two other pics on homepage pc end and added one event pic on the mobile end

---
+++ 03-frontend/react-event/src/layouts/HomePage/Carousel.tsx
@@ -0,0 +1,80 @@
+export const Carousel = () => {
+    return (
+        <div className='container mt-5' style={{ height: 550 }}>
+
+        <div className='homepage-carousel-title'>
+            <h3>Find your next fav event.</h3>
+        </div>
+
+        <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 
+            d-none d-lg-block' data-bs-interval='false'>
+
+            {/* Desktop */}
+            <div className='carousel-inner'>
+                <div className='carousel-item active'>
+                    <div className='row d-flex justify-content-center align-items-center'>
+                      <img src={require('./../../Images/EventsImages/event1.jpg')}
+                      width='151'
+                      height='233'
+                      alt="image"
+                      />
+                      <h6 className='mt-2'>Some kind of Event</h6>
+                      <p>Events</p>
+                      <a className='btn main-color text-white' href='8'>Reserve</a>
+                    </div>
+                </div>
+
+                <div className='carousel-item active'>
+                    <div className='row d-flex justify-content-center align-items-center'>
+                      <img src={require('./../../Images/EventsImages/event2.jpg')}
+                      width='151'
+                      height='233'
+                      alt="image"
+                      />
+                      <h6 className='mt-2'>Some kind of Event</h6>
+                      <p>Events</p>
+                      <a className='btn main-color text-white' href='8'>Reserve</a>
+                    </div>
+                </div>
+
+                <div className='carousel-item active'>
+                    <div className='row d-flex justify-content-center align-items-center'>
+                      <img src={require('./../../Images/EventsImages/event3.jpg')}
+                      width='151'
+                      height='233'
+                      alt="image"
+                      />
+                      <h6 className='mt-2'>Some kind of Event</h6>
+                      <p>Events</p>
+                      <a className='btn main-color text-white' href='8'>Reserve</a>
+                    </div>
+                </div>
+            </div>
+            
+            <button className='carousel-control-prev' type='button'
+                data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                <span className='visually-hidden'>Previous</span>
+            </button>
+            <button className='carousel-control-next' type='button'
+                data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                <span className='visually-hidden'>Next</span>
+            </button>
+
+            
+        </div>
+
+        {/* Mobile */}
+        <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    {/* <ReturnBook book={books[7]} key={books[7].id}/> */}
+                </div>
+            </div>
+            <div className='homepage-carousel-title mt-3'>
+                {/* <Link className='btn btn-outline-secondary btn-lg' to='/search'>View More</Link> */}
+            </div>
+        </div>
+    );
+
+}
