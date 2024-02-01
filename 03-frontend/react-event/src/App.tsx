408024670274f9d288eca18f74976230b475598b
Emily <emily.zy.lin@gmail.com>
2024-01-31 23:10:51 -0600

    added the footer.tsx

--- 03-frontend/react-event/src/App.tsx
+++ 03-frontend/react-event/src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from './layouts/NavBarAndFooter/Navbar';
 import { ExploreTopEvents } from './layouts/HomePage/ExploreTopEvents';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { Heros } from './layouts/HomePage/Heros';
+import { EventServices } from './layouts/HomePage/EventService';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <ExploreTopEvents />    
       <Carousel />
       <Heros />
-
+      <EventServices />
     </div>
     //must have the div tag for App.tsx
   );
