20268313f48439d0ca90f64f86afae4acc4c272c
Emily <emily.zy.lin@gmail.com>
2024-01-31 23:34:19 -0600

    accidently moved navbar and footer to the homepage.tsx. Have moved back

--- 03-frontend/react-event/src/App.tsx
+++ 03-frontend/react-event/src/App.tsx
@@ -1,10 +1,16 @@
 import { Homepage } from './layouts/HomePage/Homepage'
+import { Navbar } from './layouts/NavBarAndFooter/Navbar';
+import { Footer } from './layouts/NavBarAndFooter/Footer';
 import './App.css';
 
 function App() {
   return (
+    <div>
+    <Navbar />
     <Homepage />
+    <Footer />
     //must have the div tag for App.tsx
+    </div>
   );
 }
 
--- 03-frontend/react-event/src/layouts/HomePage/Homepage.tsx
+++ 03-frontend/react-event/src/layouts/HomePage/Homepage.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
-import { Navbar } from '../../layouts/NavBarAndFooter/Navbar';
-import { Footer } from '../../layouts/NavBarAndFooter/Footer';
 import { ExploreTopEvents } from '../../layouts/HomePage/components/ExploreTopEvents';
 import { Carousel } from '../../layouts/HomePage/components/Carousel';
 import { Heros } from '../../layouts/HomePage/components/Heros';
 import { EventServices } from '../../layouts/HomePage/components/EventService';
 
-
-
 export const Homepage = () => {
     return (
         <div>
-            <Navbar />
             <ExploreTopEvents />
             <Carousel />
             <Heros />
             <EventServices />
-            <Footer />
         </div>
     )
 }
