899e2e6fbc0b06fbe07689f1be6834193895c9e8
Emily <emily.zy.lin@gmail.com>
2024-02-02 16:18:17 -0600

    added constructor to the event model

--- 03-frontend/react-event/src/models/EventModel.ts
+++ 03-frontend/react-event/src/models/EventModel.ts
@@ -8,4 +8,14 @@ class EventModel {
     category?: string;
     img?: string;
     //the question mark means the value can be null
+
+    constructor(id: number, title: string, speaker: string, description: string, seats: number, seatsAvailable: number, category: string, img: string) {
+        this.id = id;
+        this.title = title;
+        this.speaker = speaker;
+        this.description = description;
+        this.seats = seats;
+        this.seatsAvailable = seatsAvailable;
+        this.category = category;
+        this.img = img;
 }
