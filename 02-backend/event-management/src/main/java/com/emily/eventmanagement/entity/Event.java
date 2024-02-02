b6d3c170e790e7f415658dc8893f47867e02b061
Emily <emily.zy.lin@gmail.com>
2024-02-01 22:12:03 -0600

    set up id in the event entity

--- 02-backend/event-management/src/main/java/com/emily/eventmanagement/entity/Event.java
+++ 02-backend/event-management/src/main/java/com/emily/eventmanagement/entity/Event.java
@@ -3,4 +3,12 @@ package com.emily.eventmanagement.entity;
 import lombok.Data;
 import jakarta.persistence.*;
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+
 }
