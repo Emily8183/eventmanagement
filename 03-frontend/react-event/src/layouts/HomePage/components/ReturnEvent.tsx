c1ff0b3bd4a2b20838839dd688224430397201b2
Emily <emily.zy.lin@gmail.com>
2024-02-03 19:07:52 -0600

    updated ReturnEvent

--- 03-frontend/react-event/src/layouts/HomePage/components/ReturnEvent.tsx
+++ 03-frontend/react-event/src/layouts/HomePage/components/ReturnEvent.tsx
@@ -2,14 +2,14 @@ import {useState, useEffect} from 'react';
 import EventModel from '../../../models/EventModel';
 // this is for dynamic page
 
-export const ReturnEvent: React.FC<{book:EventModel}> = (props) => {
+export const ReturnEvent: React.FC<{event:EventModel}> = (props) => {
     return (
     <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
         <div className='text-center'>
             {/* if else statement to display either the image called from the API or a default image */}
-            {props.book.img ? 
+            {props.event.img ? 
                 <img
-                src = {props.book.img}
+                src = {props.event.img}
                 width='151'
                 height='233'
                 alt="event"
@@ -21,8 +21,8 @@ export const ReturnEvent: React.FC<{book:EventModel}> = (props) => {
                 alt="event"
             />
             }
-            <h6 className='mt-2'>Event</h6>
-            <p>Events</p>
+            <h6 className='mt-2'>{props.event.title}</h6>
+            <p>{props.event.speaker}</p>
             <a className='btn main-color text-white' href='8'>Reserve</a>
         </div>
     </div>
