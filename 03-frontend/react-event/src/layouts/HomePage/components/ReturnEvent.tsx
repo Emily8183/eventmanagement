2c7ee010c5d71b4e6fcd4597c79395d1847c51b8
Emily <emily.zy.lin@gmail.com>
2024-02-03 19:02:43 -0600

    if else statement to display either the image called from the API or a default image

--- 03-frontend/react-event/src/layouts/HomePage/components/ReturnEvent.tsx
+++ 03-frontend/react-event/src/layouts/HomePage/components/ReturnEvent.tsx
@@ -1,15 +1,27 @@
 import {useState, useEffect} from 'react';
+import EventModel from '../../../models/EventModel';
 // this is for dynamic page
 
-export const ReturnEvent = () => {
+export const ReturnEvent: React.FC<{book:EventModel}> = (props) => {
     return (
     <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
         <div className='text-center'>
-            <img src={require('../../.././Images/EventsImages/event1.jpg')}
+            {/* if else statement to display either the image called from the API or a default image */}
+            {props.book.img ? 
+                <img
+                src = {props.book.img}
+                width='151'
+                height='233'
+                alt="event"
+            /> : 
+            <img
+                className='img-fluid'
+                img src={require('../../.././Images/EventsImages/event1.jpg')}
                 width='151'
                 height='233'
                 alt="event"
             />
+            }
             <h6 className='mt-2'>Event</h6>
             <p>Events</p>
             <a className='btn main-color text-white' href='8'>Reserve</a>
