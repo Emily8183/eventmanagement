d6ac1ad0418824905c0830e514c5b57aa6f8a308
Emily <emily.zy.lin@gmail.com>
2024-02-05 21:38:36 -0600

    added react router to App.tsx

--- 03-frontend/react-event/src/App.tsx
+++ 03-frontend/react-event/src/App.tsx
@@ -1,13 +1,27 @@
 import { Homepage } from './layouts/HomePage/Homepage'
 import { Navbar } from './layouts/NavBarAndFooter/Navbar';
 import { Footer } from './layouts/NavBarAndFooter/Footer';
+import {Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 
 export const App = () => {
   return (
     <div>
     <Navbar />
-    <Homepage />
+      <div>
+        <Switch>
+        {/* <Switch> wraps the <Route> components. */}
+
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+
+        <Route path='/home'>
+          <Homepage />
+        </Route>
+
+        </Switch>
+      </div>
     <Footer />
     </div>
   );
