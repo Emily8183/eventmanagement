fc65852053178d7919e02108563f703e30563083
Emily <emily.zy.lin@gmail.com>
2024-02-07 14:05:08 -0600

    displaying all the evnets' details on /search

--- 03-frontend/react-event/src/layouts/SearchEventsPage/Components/SearchEvent.tsx
+++ 03-frontend/react-event/src/layouts/SearchEventsPage/Components/SearchEvent.tsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import EventModel from "../../../models/EventModel";
 
-export const SearchEvent: React.FC<{event:EventModel}> = (props) => {
+export const SearchEvent: React.FC<{ event: EventModel }> = (props) => {
     return (
         <div className='card mt-3 shadow p-3 mb-3 bg-body rounded'>
             <div className='row g-0'>
@@ -36,10 +37,29 @@ export const SearchEvent: React.FC<{event:EventModel}> = (props) => {
                             />
                         }
                     </div>
-                </div>
-           </div>
-        </div>
+                    </div>
 
+                    <div className='col-md-6'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>
+                                {props.event.speaker}
+                            </h5>
+                            <h4>
+                                {props.event.title}
+                            </h4>
+                            <p className='card-text'>
+                                {props.event.description}
+                            </p>
+                        </div>
+                    </div>
+                    <div className='col-md-4 d-flex justify-content-center align-items-center'>
+                        <Link className='btn btn-md main-color text-white' to={`/checkout/${props.event.id}`}>
+                            View Details
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        
     );
 
 }
