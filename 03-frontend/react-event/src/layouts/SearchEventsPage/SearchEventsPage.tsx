f8959152218dc2a1fbca347b3d83f1f0174fd633
Emily <emily.zy.lin@gmail.com>
2024-02-07 11:18:17 -0600

    set up useEffect in SearchEventsPage

--- 03-frontend/react-event/src/layouts/SearchEventsPage/SearchEventsPage.tsx
+++ 03-frontend/react-event/src/layouts/SearchEventsPage/SearchEventsPage.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EventModel from "../../models/EventModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const searchEventsPage = () => {
 
@@ -10,7 +11,69 @@ export const searchEventsPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
  
+    useEffect(() => {
+        //fetch data from api
+        const fetchEvents = async () => {
+            const baseUrl: string = "http://localhost:8080/api/events";
+            const url: string = `${baseUrl}?page=0&size=20`;
+            // NOTE: must use back tick ` instead of ""
+            const response= await fetch(url);
 
+            //if else statement to see if needs to throw an error
+            if(!response) {
+                throw new Error('Somthing went wrong!');
+            }
+
+        //save data to responseData
+        const responseJson = await response.json();
+        const responseData = responseJson._embedded.events;
+
+        // set up a new variable, then run a loop to push the data from the api(responseData) to the variable
+        const loadedEvents: EventModel[] = [];
+
+        for (const key in responseData) {
+            loadedEvents.push(
+                {
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    speaker: responseData[key].speaker,
+                    description: responseData[key].description,
+                    tickets: responseData[key].tickets,
+                    ticketsAvailable: responseData[key].ticketsAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img,
+                });
+        }
+
+        // apply the useEffect to "setEvents"
+        setEvents(loadedEvents);
+        //after loading, change the loading status to false;
+        setIsLoading(false);
+
+    };
+
+    fetchEvents().catch((error:any) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+    })    
+        
+    },[]);
+
+    if (isLoading) {
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+ 
 
 
 }
