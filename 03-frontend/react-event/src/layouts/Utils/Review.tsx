24848f333818e5d1b0923fad2076b106163d4622
Emily <emily.zy.lin@gmail.com>
2024-02-14 21:37:16 -0600

    set up the date in the review function

--- 03-frontend/react-event/src/layouts/Utils/Review.tsx
+++ 03-frontend/react-event/src/layouts/Utils/Review.tsx
@@ -1,1 +1,15 @@
-export const Review: React.FC<{ review: ReviewModel }> = ({ prop }) => {};
+import ReviewModel from "../../models/ReviewModel";
+
+export const Review: React.FC<{ review: ReviewModel }> = (props) => {
+  const date = new Date(props.review.date);
+  const longMonth = date.toLocaleString("en-us", { month: "long" });
+
+  const dateDay = date.getDate();
+  const dateYear = date.getFullYear();
+
+  const dateRender = longMonth + "" + dateDay + "," + dateYear;
+
+  return(
+    
+  );
+};
