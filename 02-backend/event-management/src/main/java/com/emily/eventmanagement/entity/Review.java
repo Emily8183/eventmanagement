040561fa21851a66ba2f16440c8301cb2151436a
Emily <emily.zy.lin@gmail.com>
2024-02-14 22:40:25 -0600

    creating entity of review, set up id

---
+++ 02-backend/event-management/src/main/java/com/emily/eventmanagement/entity/Review.java
@@ -0,0 +1,27 @@
+package com.emily.eventmanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import javax.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name="review")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+
+
+
+
+
+}
