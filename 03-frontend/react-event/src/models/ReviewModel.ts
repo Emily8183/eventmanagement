0a0ec7d7337f8ab387c132be261ced74adbc38d3
Emily <emily.zy.lin@gmail.com>
2024-02-14 20:37:13 -0600

    created the model for review

--- 03-frontend/react-event/src/models/ReviewModel.ts
+++ 03-frontend/react-event/src/models/ReviewModel.ts
@@ -1,8 +1,24 @@
-class ReviewModel {
+export default class ReviewModel {
   id: number;
   userEmail: string;
   date: string;
   rating: number;
   event_id: number;
   reviewDescription?: string;
+
+  constructor(
+    id: number,
+    userEmail: string,
+    date: string,
+    rating: number,
+    event_id: number,
+    reviewDescription: string
+  ) {
+    this.id = id;
+    this.userEmail = userEmail;
+    this.date = date;
+    this.rating = rating;
+    this.event_id = event_id;
+    this.reviewDescription = reviewDescription;
+  }
 }
