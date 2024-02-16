d5079c83f111f60040b9050a25aa500d8c9d8ae1
Emily <emily.zy.lin@gmail.com>
2024-02-16 14:44:04 -0600

    added findByBookId into ReviewRepository

--- 02-backend/event-management/src/main/java/com/emily/eventmanagement/dao/ReviewRepository.java
+++ 02-backend/event-management/src/main/java/com/emily/eventmanagement/dao/ReviewRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Review findByBookId(@RequestParam("book_id") Long bookId);
     Review findByUserEmailAndEventId(String userEmail, Long eventId);
 }
