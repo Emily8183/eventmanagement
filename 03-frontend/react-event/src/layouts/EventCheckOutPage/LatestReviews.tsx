d028d23127ef65baf07384dc802fa6ffc4941b5c
Emily <emily.zy.lin@gmail.com>
2024-02-27 17:29:22 -0600

    added the display when the reviews' length is longer than 0 or not

--- 03-frontend/react-event/src/layouts/EventCheckOutPage/LatestReviews.tsx
+++ 03-frontend/react-event/src/layouts/EventCheckOutPage/LatestReviews.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import ReviewModel from "../../models/ReviewModel";
+import { Review } from "../Utils/Review";
 
 export const LatestReviews: React.FC<{
   reviews: ReviewModel[];
@@ -11,7 +13,29 @@ export const LatestReviews: React.FC<{
         <h2>Latest Reviews: </h2>
       </div>
 
-      <div className="col-sm-10 col-md-10"></div>
+      <div className="col-sm-10 col-md-10">
+        {props.reviews.length > 0 ? (
+          <>
+            {props.reviews.slice(0, 3).map((eachReview) => (
+              <Review review={eachReview} key={eachReview.id}></Review>
+            ))}
+
+            <div className="m-3">
+              <Link
+                type="button"
+                className="btn main-color btn-md text-white"
+                to={`/reviewlist/${props.bookId}`}
+              >
+                Reach all reviews.
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="m-3">
+            <p className="lead">Currently there are no reviews for this book</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
