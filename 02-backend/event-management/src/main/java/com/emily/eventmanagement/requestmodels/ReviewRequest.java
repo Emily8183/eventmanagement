e8964cff84dc68dd11195c858c5b2e6bab96ab61
Emily <emily.zy.lin@gmail.com>
2024-02-28 12:27:59 -0600

    added reviewDescription to reviewRequest model

--- 02-backend/event-management/src/main/java/com/emily/eventmanagement/requestmodels/ReviewRequest.java
+++ 02-backend/event-management/src/main/java/com/emily/eventmanagement/requestmodels/ReviewRequest.java
@@ -2,12 +2,14 @@ package com.emily.eventmanagement.requestmodels;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 public class ReviewRequest {
     private double rating;
 
     private Long eventId;
 
-
+    private Optional<String> reviewDescription;
 
 }
