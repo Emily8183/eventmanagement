a3be2852565377ef21242cd86f08aa86116283bb
Emily <emily.zy.lin@gmail.com>
2024-03-01 16:41:27 -0600

    added pkce to secure that the authorization code is secure

--- 03-frontend/react-event/src/lib/oktaConfig.ts
+++ 03-frontend/react-event/src/lib/oktaConfig.ts
@@ -4,4 +4,7 @@ export const oktaConfig = {
   //get the devl URL location and clientId from okta
   redirectUri: "http://localhost:3000/login/callback",
   //sign-in redirect URIs
+  scopes: ["openid", "email", "profile"],
+  pkce: true,
+  //PKCE is a security feature that helps to ensure that the authorization code that is sent back to your application is secure.
 };
