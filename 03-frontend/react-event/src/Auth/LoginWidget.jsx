b9d61dccee0253fca8dbec1d9b960a1093e77de0
Emily <emily.zy.lin@gmail.com>
2024-03-03 23:37:49 -0600

    set up error msg in LoginWidget

---
+++ 03-frontend/react-event/src/Auth/LoginWidget.jsx
@@ -0,0 +1,10 @@
+const LoginWidget = ({ config }) => {
+  const { oktaAuth, authState } = useOktaAuth();
+  const onSuccess = (tokens) => {
+    oktaAuth.handleLoginRedirect(tokens);
+  };
+
+  const onError = (err) => {
+    console.log("Sign in error: ", err);
+  };
+};
