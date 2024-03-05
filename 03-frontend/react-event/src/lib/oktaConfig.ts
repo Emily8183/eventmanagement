e2a9061f6d483e2b2fe1af527ee1b8b363aa96a2
Emily <emily.zy.lin@gmail.com>
2024-03-05 16:43:39 -0600

    corrected the issuer from http to https

--- 03-frontend/react-event/src/lib/oktaConfig.ts
+++ 03-frontend/react-event/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
   clientId: "0oafgsq7k5l2tMiJ45d7",
-  issuer: "http://dev-90641321.okta.com/oauth2/default",
+  issuer: "https://dev-90641321.okta.com/oauth2/default",
   //get the devl URL location and clientId from okta
   redirectUri: "http://localhost:3000/login/callback",
   //sign-in redirect URIs
