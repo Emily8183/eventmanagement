f70e83d013cf420d74af8095b67718253e9b6150
Emily <emily.zy.lin@gmail.com>
2024-03-06 23:34:08 -0600

    added the authentication status for the mobile end

--- 03-frontend/react-event/src/layouts/HomePage/components/Heros.tsx
+++ 03-frontend/react-event/src/layouts/HomePage/components/Heros.tsx
@@ -63,9 +63,19 @@ export const Heros = () => {
                 The Preplingo team would love to know what you have been
                 enjoying. we will be able to organize more fun events for you!
               </p>
-              <a className="btn main-color btn-lg text-white" href="#">
-                Sign up
-              </a>
+              {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="search"
+                >
+                  Explore top books
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="/login">
+                  Sign up
+                </Link>
+              )}
             </div>
           </div>
           <div className="m-2">
